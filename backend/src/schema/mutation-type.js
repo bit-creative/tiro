2d6d5580846a5cf4d3de28a465c0b3187bfe41b0
George Bockari <bitwhys@gmail.com>
2020-04-21 05:24:24 -0700

    refactor(backend/mutation-type): add user authentication mutations

--- backend/src/schema/mutation-type.js
+++ backend/src/schema/mutation-type.js
@@ -1,8 +1,29 @@
 import { gql } from "apollo-server-express"
 
 const mutationTypeDef = gql`
+  input CaptureEmailInput {
+    email: String!
+  }
+  input SignupInput {
+    id: String!
+    firstName: String!
+    lastName: String!
+  }
+
+  input LoginInput {
+    email: String!
+    password: String!
+  }
+
+  type AuthResponse {
+    token: String!
+    user: User!
+  }
+
   type Mutation {
-    login: String
+    captureEmail(input: CaptureEmailInput): User
+    signup(input: SignupInput): AuthResponse!
+    login(input: LoginInput): AuthResponse!
   }
 `
 
